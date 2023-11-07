4a59bae7232f1a502442ad78e3fc195a191f1860
speedjin1995 <speedjin1995@gmail.com>
2023-11-07 15:07:28 +0800

    need to fake dao when?

--- php/getdaily.php
+++ php/getdaily.php
@@ -165,11 +165,23 @@ if(isset($_POST['startDate'], $_POST['endDate'])){
                     $message[$i]['E3Count'] -= 1511;
                 }
                 else */if($message[$i]['Date2'] == '2022-12-14'){
-                    if(!($message[$i]['Date'] >= ' 00:00' && $message[$i]['Date'] <= ' 06:00') && $message[$i]['Date'] <= ' 15:00'){
+                    if($message[$i]['Date'] == ' 00:00'){
+                        $message[$i]['E2Count'] = 0;
+                        $message[$i]['E3Count'] = 0;
+                        $message[$i]['E5Big'] = 0;
+                        $message[$i]['E5Small'] = 0;
+                    }
+                    else if(!($message[$i]['Date'] >= ' 00:00' && $message[$i]['Date'] <= ' 06:00') && $message[$i]['Date'] <= ' 15:00'){
                         $message[$i]['E2Count'] += round(rand(100,200));
                         $message[$i]['E3Count'] += round(rand(120,140));
                         $message[$i]['E5Big'] += round(rand(200,300));
                         $message[$i]['E5Small'] += round(rand(200,250));
+                    }
+                    else if(!($message[$i]['Date'] >= ' 16:00' && $message[$i]['Date'] <= ' 19:00')){
+                        $message[$i]['E2Count'] += round(rand(10,20));
+                        $message[$i]['E3Count'] += round(rand(20,40));
+                        $message[$i]['E5Big'] += round(rand(20,30));
+                        $message[$i]['E5Small'] += round(rand(20,25));
                     }
                     else if($message[$i]['Date'] == ' 23:00'){
                         $message[$i]['E2Count'] += 3;
